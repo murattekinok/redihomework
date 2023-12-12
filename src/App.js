f413b6a990253333eb540cf5daae1e0efa0e7d81
Murat Tekinok <tekinokmurat@gmail.com>
2023-12-12 14:49:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import ColorButton from './ColorButton';
+// import withColor from './withColor';
 import './App.css';
+
+// const ColorButtonWithColor = withColor(ColorButton); 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ColorButton />
     </div>
   );
 }
